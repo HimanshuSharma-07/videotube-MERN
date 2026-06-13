b6128e32798421597d4183e4db1169af17daf9fc
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-06-14 01:09:06 +0530

    feat: implement new routes for dashboard, healthcheck, playlist, subscription, and tweet management with JWT verification

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,25 +1,22 @@
-// import { Router } from "express"
-// import { verifyJWT } from "../middleware/auth.middleware.js"
-// import { 
-//     addComment,
-//     deleteComment,
-//     getVideoCommentByVideoId,
-//     getVideoComments,
-//     updateComment,
+import { Router } from "express"
+import { verifyJWT } from "../middleware/auth.middleware.js"
+import { 
+    addComment,
+    deleteComment,
+    getVideoComments,
+    updateComment,
+} from "../controllers/comment.controller.js"
 
-// } from "../controllers/comment.controller.js"
+const router = Router();
 
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-// const router = Router();
+router.route("/:videoId")
+    .get(getVideoComments)
+    .post(addComment);
 
+router.route("/c/:commentId")
+    .patch(updateComment)
+    .delete(deleteComment);
 
-// // router.route("/video/:videoId").get(getVideoCommentByVideoId)
-
-
-// router.route("/add-comment").post(verifyJWT, addComment)
-// router.route("/update/:commentId").patch(verifyJWT, updateComment)
-// router.route("/delete/:commentId").delete(verifyJWT, deleteComment)
-
-
-
-// export default router;
+export default router;
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,20 +1,18 @@
-import { Router } from "express"
-import { verifyJWT } from "../middleware/auth.middleware"
-import { toggleLike } from "../controllers/like.controller";
-
+import { Router } from 'express';
+import {
+    getLikedVideos,
+    toggleCommentLike,
+    toggleVideoLike,
+    toggleTweetLike,
+} from "../controllers/like.controller.js"
+import {verifyJWT} from "../middleware/auth.middleware.js"
 
 const router = Router();
+router.use(verifyJWT); 
 
+router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
+router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/videos").get(getLikedVideos);
 
-router.route("video/:videoId")
-    .post(verifyJWT, toggleLike)
-
-
-router.route("/comments/:commentId")
-    .post(verifyJWT, toggleLike)
-
-router.route("/comments/:tweetId")
-    .post(verifyJWT, toggleLike)
-
-
-export default router
+export default router;
